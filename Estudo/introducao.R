b250d858d3f44848993f4878b5a034f2ea14de54
lunadis <luislopes116@outlook.com>
2018-04-18 12:26:40 -0300

    introdução a linguagem R

--- Estudo/introducao.R
+++ Estudo/introducao.R
@@ -14,7 +14,7 @@ div <- x / y
 exp <- x ^ y
 
 
-#Exibir no console
+#Para exibir no console basta colocar o nome da váriavel
 
 char
 soma
@@ -31,7 +31,7 @@ vetor_poker <- c(140,-50,20,-120,240)
 #Vetor indicando ganhos e perdas por dias da semana no poker
 vetor_roleta <- c(-24,-50,100,-350,10)
 
-#vetor para indicar/nomear os dias da semana com a função [names()]
+#vetor para indicar/nomear os dias da semana com a comando [names()]
 vetor_dias <- c("segunda","terça","quarta","quinta","sexta")
 
 #fazendo a identificação dos elementos dos vetores usando [names()] e o vetor dos dias
@@ -41,3 +41,37 @@ names(vetor_roleta) <- vetor_dias
 
 vetor_poker
 vetor_roleta
+
+#calculando total por dia
+
+ganhos_dia <- vetor_poker + vetor_roleta
+
+ganhos_dia
+
+#Soma dos componentes dos vetores usando comando [sum]
+
+total_poker <- sum(vetor_poker)
+total_roleta <- sum(vetor_roleta)
+total_semana <- sum(ganhos_dia)
+
+total_poker
+total_roleta
+total_semana
+
+#Soma entre componentes dos vetores
+
+total_quarta <- vetor_poker[3] + vetor_roleta[3]
+
+total_quarta
+
+
+#Usando operadores de comparação para verificação booleana
+total_poker > total_roleta
+total_poker < total_roleta
+total_poker == total_roleta
+total_poker != total_roleta
+total_poker >= total_roleta
+total_poker <= total_roleta
+
+#Usando operadores condicionais e controle de fluxo
+
